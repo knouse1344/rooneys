793fd9605c988ee622c9dc9ac7e155b24d9c5f3f
Bryan <bknouse@u.rochester.edu>
2013-06-07 10:47:05 -0400

    first commit for rooneys

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rooneys::Application.config.secret_token = '76e93f56b9890ddbd0f2a5d460478457cde024f56fe09784667a5daa28ccd49345c84c51e73b9780b5fe3daf63726b5898148844335889a91844c36360866b24'
