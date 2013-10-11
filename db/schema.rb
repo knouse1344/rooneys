c1983701ad7fe89b1cb2c0ead6ebb4d2ae117457
Bryan <bknouse@u.rochester.edu>
2013-10-10 22:04:17 -0400

    db stuff

--- db/schema.rb
+++ db/schema.rb
@@ -45,24 +45,17 @@ ActiveRecord::Schema.define(:version => 20130730130438) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
-  
-  create_table "dinnersections", :force => true do |t|
-    t.string   "identity"
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
   create_table "dinneritems", :force => true do |t|
     t.string   "desc"
     t.string   "name"
     t.string   "price"
     t.integer  "dinnersection_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
-  create_table "winesections", :force => true do |t|
+  create_table "dinnersections", :force => true do |t|
     t.string   "identity"
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -78,4 +71,10 @@ ActiveRecord::Schema.define(:version => 20130730130438) do
     t.datetime "updated_at",      :null => false
   end
 
+  create_table "winesections", :force => true do |t|
+    t.string   "identity"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end  
 end
