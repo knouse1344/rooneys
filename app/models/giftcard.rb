6cc6740637c6964556acd633ca8550ff434af5b1
Bryan <bknouse@u.rochester.edu>
2013-12-04 12:30:48 -0500

    form validations working, giftcard ready to go

--- app/models/giftcard.rb
+++ app/models/giftcard.rb
@@ -1,4 +1,6 @@
 class Giftcard < ActiveRecord::Base
 	attr_accessible :name, :email, :phone, :address, :city, :state, :zip, :buyername, :cardtype, :cardnum, :amount, :expiration, :comments
 	
+	validates_presence_of :name, :email, :phone, :address, :city, :state, :zip, :buyername, :cardtype, :cardnum, :amount, :expiration
+
 end
